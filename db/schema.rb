e527f98bb416548d35947d4698439976116f86a6
NataliaColome <nmcolome@gmail.com>
2017-05-25 17:49:26 -0600

    create user migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525234146) do
+ActiveRecord::Schema.define(version: 20170525234808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20170525234146) do
   create_table "schools", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "password_digest"
+    t.integer "role"
+    t.integer "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
