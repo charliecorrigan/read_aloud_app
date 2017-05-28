e4bf7e82f6a8d2a8a0e426f5329d2c0f49d38b4d
NataliaColome <nmcolome@gmail.com>
2017-05-27 18:17:07 -0600

    separate welcome views for staff and users through decorator pattern

--- spec/features/user_navigates_welcome_page_spec.rb
+++ spec/features/user_navigates_welcome_page_spec.rb
@@ -4,14 +4,13 @@ RSpec.describe "User navigates welcome page" do
   it "they click on Read" do
     user = User.create(first_name: "Hermione",
                       last_name: "Granger",
-                      active: true,
                       username: "hgranger",
                       password: "supersecretpassword",
                       role: 0,
                       language: 0)
-    visit root
-    fill_in "username", with: user.username
-    fill_in "password", with: "supersecretpassword"
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "supersecretpassword"
     click_on "Sign In"
     click_on "Read"
 
@@ -21,34 +20,48 @@ RSpec.describe "User navigates welcome page" do
   it "they click on History" do
     user = User.create(first_name: "Hermione",
                       last_name: "Granger",
-                      active: true,
                       username: "hgranger",
                       password: "supersecretpassword",
                       role: 0,
                       language: 0)
-    visit root
-    fill_in "username", with: user.username
-    fill_in "password", with: "supersecretpassword"
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "supersecretpassword"
     click_on "Sign In"
     click_on "History"
 
-    expect(current_path).to eq(edit_user(user))
+    expect(current_path).to eq(user_events_path(user))
   end
 
   it "they click on Sign Out" do
     user = User.create(first_name: "Hermione",
                       last_name: "Granger",
-                      active: true,
                       username: "hgranger",
                       password: "supersecretpassword",
                       role: 0,
                       language: 0)
-    visit root
-    fill_in "username", with: user.username
-    fill_in "password", with: "supersecretpassword"
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "supersecretpassword"
     click_on "Sign In"
     click_on "Sign Out"
 
-    expect(current_path).to eq(root)
+    expect(current_path).to eq(login_path)
+  end
+
+  it "can't access Manage or Profile" do
+    user = User.create(first_name: "Hermione",
+                      last_name: "Granger",
+                      username: "hgranger",
+                      password: "supersecretpassword",
+                      role: 0,
+                      language: 0)
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "supersecretpassword"
+    click_on "Sign In"
+
+    expect(page).to_not have_content("Manage")
+    expect(page).to_not have_content("Profile")
   end
 end
