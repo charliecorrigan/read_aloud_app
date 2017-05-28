74aaacec32fc0b0a19e9090d15a735af7d1e1870
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-05-28 11:31:54 -0600

    Add development seed file

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,5 +1,5 @@
 class Classroom < ApplicationRecord
   belongs_to :school
-  has_many :user_classrooms
+  has_many :user_classrooms, :dependent => :destroy
   has_many :users, through: :user_classrooms
 end
