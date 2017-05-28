f475783bc9ac387fa4b55ad7d34703a64f0577e0
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-05-28 07:16:07 -0600

    Add table to user/index and add enums for role to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  enum role: [:volunteer, :staff, :admin]
+
   has_many :user_classrooms
   has_many :classrooms, through: :user_classrooms
   has_many :events, through: :user_classrooms
--- spec/features/staff_views_all_users_spec.rb
+++ spec/features/staff_views_all_users_spec.rb
@@ -6,36 +6,35 @@ RSpec.describe "Staff views all users" do
     class1 = school.classrooms.create(teacher_name: "Prof. Snape", grade_level: "ECE", number_of_students_enrolled: 20)
     staff = User.create(first_name: "Minerva",
                       last_name: "McGonagall",
-                      active: true,
                       username: "mmcgonagall",
                       password: "supersecretpassword",
                       role: 1,
                       language: 0)
     volunteer = User.create(first_name: "Hermione",
                       last_name: "Granger",
-                      active: true,
                       username: "hgranger",
                       password: "supersecretpassword",
                       role: 0,
                       language: 0)
 
-    visit root
-    fill_in "username", with: staff.username
-    fill_in "password", with: "supersecretpassword"
+    visit login_path
+    fill_in "Username", with: staff.username
+    fill_in "Password", with: "supersecretpassword"
     click_on "Sign In"
 
     click_on "Manage"
     click_on "View All Volunteers"
 
-    expect(current_path).to eq(staff_users_path(staff))
+    save_and_open_page
+    expect(current_path).to eq(users_path)
     expect(page).to have_content("Hermione")
     expect(page).to have_content("Granger")
     expect(page).to have_content("hgranger")
-    expect(page).to have_content("Volunteer")
+    expect(page).to have_content("volunteer")
 
     expect(page).to have_content("Minerva")
     expect(page).to have_content("McGonagall")
     expect(page).to have_content("mmcgonagall")
-    expect(page).to have_content("Staff")
+    expect(page).to have_content("staff")
   end
 end
