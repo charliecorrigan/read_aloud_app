f84245f1506f6f8edfa705d501e72d807e6ac205
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-05-28 06:59:18 -0600

    Add users#destroy action and link

--- spec/features/staff_deletes_volunteer_spec.rb
+++ spec/features/staff_deletes_volunteer_spec.rb
@@ -4,30 +4,28 @@ RSpec.describe "Staff deletes a volunteer" do
   it "they delete an existing volunteer" do
     staff = User.create(first_name: "Minerva",
                       last_name: "McGonagall",
-                      active: true,
                       username: "mmcgonagall",
                       password: "supersecretpassword",
                       role: 1,
                       language: 0)
     volunteer = User.create(first_name: "Hermione",
                       last_name: "Granger",
-                      active: true,
                       username: "hgranger",
                       password: "supersecretpassword",
                       role: 0,
                       language: 0)
 
-    visit root
-    fill_in "username", with: staff.username
-    fill_in "password", with: "supersecretpassword"
+    visit login_path
+    fill_in "Username", with: staff.username
+    fill_in "Password", with: "supersecretpassword"
     click_on "Sign In"
 
     click_on "Manage"
     click_on "View All Volunteers"
     click_on "Hermione Granger"
-    click_on "Delete Volunteer"
+    click_on "Delete Account"
 
-    expect(current_path).to eq(staff_users_path(staff))
+    expect(current_path).to eq(users_path)
     expect(page).to_not have_content("Hermione")
     expect(page).to_not have_content("Granger")
     expect(page).to have_content("Minerva")
